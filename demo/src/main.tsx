f30a7615bfeb02978962e12182452952f283e8f4
Zachary Gagnon <zachary.gagnon@mentorcliq.com>
2026-04-26 04:22:14 -0400

    Delete unused dependencies and general cleanup

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 import App from "./App";
-import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
 import { createRoot } from "react-dom/client";
-
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn: "https://dcc8b6eb106b43fcbe6385fb491871ad@o1071232.ingest.sentry.io/6068046",
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
-}
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
--- demo/src/pages/Editor/index.tsx
+++ demo/src/pages/Editor/index.tsx
@@ -1,12 +1,9 @@
 import React, { useCallback } from "react";
-import { Button, ConfigProvider, Dropdown, Menu, PageHeader } from "@arco-design/web-react";
 import { cloneDeep } from "lodash";
 import mjml from "mjml-browser";
 
 import { saveAs } from "file-saver";
 import { EmailEditor, EmailEditorProvider, IEmailTemplate } from "easy-email-editor";
-
-import { pushEvent } from "@demo/utils/pushEvent";
 
 import { AdvancedType, BasicType, IBlockData, JsonToMjml } from "easy-email-core";
 import { ExtensionProps, StandardLayout } from "easy-email-extensions";
@@ -14,7 +11,6 @@ import { ExtensionProps, StandardLayout } from "easy-email-extensions";
 import "easy-email-editor/lib/style.css";
 import "easy-email-extensions/lib/style.css";
 import blueTheme from "@arco-themes/react-easy-email-theme/css/arco.css?inline";
-import enUS from "@arco-design/web-react/es/locale/en-US";
 
 import { useWindowSize } from "react-use";
 import { TEMPLATE_DATA } from "@demo/pages/Editor/Arturia - Newsletter";
@@ -113,7 +109,6 @@ export default function Editor() {
       context: values.content,
     });
 
-    pushEvent({ event: "MJMLExport", payload: { values } });
     navigator.clipboard.writeText(mjmlString);
     saveAs(new Blob([mjmlString], { type: "text/mjml" }), "easy-email.mjml");
   };
@@ -127,7 +122,6 @@ export default function Editor() {
 
     const html = mjml(mjmlString, {}).html;
 
-    pushEvent({ event: "HTMLExport", payload: { values } });
     navigator.clipboard.writeText(html);
     saveAs(new Blob([html], { type: "text/html" }), "easy-email.html");
   };
@@ -151,8 +145,6 @@ export default function Editor() {
     content: sourceData, // replace standard block
   };
 
-  console.log(TEMPLATE_DATA);
-
   const onSubmit = useCallback(
     async (values: IEmailTemplate) => {
       console.log(values);
@@ -163,122 +155,129 @@ export default function Editor() {
   if (!initialValues) return null;
 
   return (
-    <ConfigProvider locale={enUS}>
-      <div>
-        <style>{blueTheme}</style>
-        <EmailEditorProvider
-          enabledLogic={true}
-          height={"calc(100vh - 108px)"}
-          data={initialValues}
-          onUploadImage={(file) => {
-            console.log(file);
-            return Promise.resolve(file.text());
-          }}
-          onSubmit={onSubmit}
-          dashed={false}
-          compact={compact}
-          mergeTags={{
-            test: "test",
-            test2: [
-              {
-                label: "test2",
-                value: "test2",
-              },
-              {
-                label: "test3",
-                value: "test3",
-              },
-            ],
-            test4: {
-              label: "test4",
-              value: "test4",
-            },
-          }}
-          fontList={[
-            { label: "Arial", value: "Arial" },
-            { label: "Arial Black", value: "Arial Black" },
-            { label: "Arial Narrow", value: "Arial Narrow" },
-            { label: "Book Antiqua", value: "Book Antiqua" },
-            { label: "Calibri", value: "Calibri" },
-            { label: "Cambria", value: "Cambria" },
-            { label: "Candara", value: "Candara" },
-            { label: "Century Gothic", value: "Century Gothic" },
-            { label: "Comic Sans MS", value: "Comic Sans MS" },
-            { label: "Constantia", value: "Constantia" },
-            { label: "Corbel", value: "Corbel" },
-            { label: "Courier New", value: "Courier New" },
+    <div>
+      {/* TODO get rid of this when arco-design is no more */}
+      <style>{blueTheme}</style>
+      <EmailEditorProvider
+        enabledLogic={true}
+        height={"calc(100vh - 108px)"}
+        data={initialValues}
+        onUploadImage={(file) => {
+          console.log(file);
+          return Promise.resolve(file.text());
+        }}
+        onSubmit={onSubmit}
+        dashed={false}
+        compact={compact}
+        mergeTags={{
+          test: "test",
+          test2: [
             {
-              label: "Franklin Gothic Medium",
-              value: "Franklin Gothic Medium",
+              label: "test2",
+              value: "test2",
             },
-            { label: "Garamond", value: "Garamond" },
-            { label: "Georgia", value: "Georgia" },
-            { label: "Helvetica", value: "Helvetica" },
-            { label: "Impact", value: "Impact" },
-            { label: "Lato", value: "Lato, Arial, sans-serif" },
-            { label: "Lucida Console", value: "Lucida Console" },
-            { label: "Open Sans", value: "Open Sans, Arial, sans-serif" },
-            { label: "Palatino Linotype", value: "Palatino Linotype" },
-            { label: "Roboto", value: "Roboto, Arial, sans-serif" },
-            { label: "Segoe UI", value: "Segoe UI" },
-            { label: "Tahoma", value: "Tahoma" },
-            { label: "Times New Roman", value: "Times New Roman" },
-            { label: "Trebuchet MS", value: "Trebuchet MS" },
-            { label: "Verdana", value: "Verdana" },
-          ]}
-        >
-          {({ values }, { submit, restart }) => {
-            return (
-              <>
-                <PageHeader
-                  style={{ background: "var(--color-bg-2)" }}
-                  backIcon
-                  title="Edit"
-                  extra={
-                    <Dropdown
-                      droplist={
-                        <Menu>
-                          <Menu.Item
-                            key="Export MJML"
-                            onClick={() => onExportMJML(values)}
-                          >
-                            Export MJML
-                          </Menu.Item>
-                          <Menu.Item
-                            key="Export HTML"
-                            onClick={() => onExportHTML(values)}
-                          >
-                            Export HTML
-                          </Menu.Item>
-                          <Menu.Item
-                            key="Export JSON"
-                            onClick={() => onExportJSON(values)}
-                          >
-                            Export JSON
-                          </Menu.Item>
-                        </Menu>
-                      }
-                    >
-                      <Button>
-                        <strong>Export</strong>
-                      </Button>
-                    </Dropdown>
-                  }
-                />
-
-                <StandardLayout
-                  categories={defaultCategories}
-                  showSourceCode={true}
-                  mjmlReadOnly={false}
-                  showBlockLayer={true}
+            {
+              label: "test3",
+              value: "test3",
+            },
+          ],
+          test4: {
+            label: "test4",
+            value: "test4",
+          },
+        }}
+        fontList={[
+          { label: "Arial", value: "Arial" },
+          { label: "Arial Black", value: "Arial Black" },
+          { label: "Arial Narrow", value: "Arial Narrow" },
+          { label: "Book Antiqua", value: "Book Antiqua" },
+          { label: "Calibri", value: "Calibri" },
+          { label: "Cambria", value: "Cambria" },
+          { label: "Candara", value: "Candara" },
+          { label: "Century Gothic", value: "Century Gothic" },
+          { label: "Comic Sans MS", value: "Comic Sans MS" },
+          { label: "Constantia", value: "Constantia" },
+          { label: "Corbel", value: "Corbel" },
+          { label: "Courier New", value: "Courier New" },
+          {
+            label: "Franklin Gothic Medium",
+            value: "Franklin Gothic Medium",
+          },
+          { label: "Garamond", value: "Garamond" },
+          { label: "Georgia", value: "Georgia" },
+          { label: "Helvetica", value: "Helvetica" },
+          { label: "Impact", value: "Impact" },
+          { label: "Lato", value: "Lato, Arial, sans-serif" },
+          { label: "Lucida Console", value: "Lucida Console" },
+          { label: "Open Sans", value: "Open Sans, Arial, sans-serif" },
+          { label: "Palatino Linotype", value: "Palatino Linotype" },
+          { label: "Roboto", value: "Roboto, Arial, sans-serif" },
+          { label: "Segoe UI", value: "Segoe UI" },
+          { label: "Tahoma", value: "Tahoma" },
+          { label: "Times New Roman", value: "Times New Roman" },
+          { label: "Trebuchet MS", value: "Trebuchet MS" },
+          { label: "Verdana", value: "Verdana" },
+        ]}
+      >
+        {({ values }, { submit, restart }) => {
+          return (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "10px",
+                  background: "#FFF",
+                }}
+              >
+                <h1
+                  style={{
+                    fontFamily: "sans-serif",
+                  }}
                 >
-                  <EmailEditor />
-                </StandardLayout>
-              </>
-            );
-          }}
-        </EmailEditorProvider>
-      </div>
-    </ConfigProvider>
+                  Lattice Editor
+                </h1>
+                <div style={{ display: "flex", gap: "10px" }}>
+                  <button
+                    style={{
+                      height: "32px",
+                    }}
+                    onClick={() => onExportMJML(values)}
+                  >
+                    Export MJML
+                  </button>
+                  <button
+                    style={{
+                      height: "32px",
+                    }}
+                    onClick={() => onExportHTML(values)}
+                  >
+                    Export HTML
+                  </button>
+                  <button
+                    style={{
+                      height: "32px",
+                    }}
+                    onClick={() => onExportJSON(values)}
+                  >
+                    Export JSON
+                  </button>
+                </div>
+              </div>
+
+              <StandardLayout
+                categories={defaultCategories}
+                showSourceCode={true}
+                mjmlReadOnly={false}
+                showBlockLayer={true}
+              >
+                <EmailEditor />
+              </StandardLayout>
+            </>
+          );
+        }}
+      </EmailEditorProvider>
+    </div>
   );
 }
